fec420a3d89a5f5ea873c953f76343e2e4e3af47
KushNee <wxtz521@gmail.com>
2021-07-08 23:53:41 +0800

    init

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,29 @@
+local status, treesitter = pcall(require, 'nvim-treesitter.configs')
+if (not status) then return end
+
+treesitter.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = false,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "toml",
+    "fish",
+    "json",
+    "yaml",
+    "html",
+    "scss",
+    "go",
+    "gomod",
+    "python",
+  },
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.used_by = { "javascript", "typescript.tsx" }
+
---
+++ lua/plugins.lua
@@ -0,0 +1,43 @@
+vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+  -- git
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+  use 'cohama/lexima.vim'
+  -- statusline
+  use { 'hoob3rt/lualine.nvim', requires = {{'kyazdani42/nvim-web-devicons'}}}
+  -- file manager
+  use {
+    'Shougo/defx.nvim',
+    requires = {'kristijanhusak/defx-git','kristijanhusak/defx-icons'},
+    run= ':UpdateRemotePlugins' 
+  }
+  -- lsp
+  use { 'glepnir/lspsaga.nvim', requires= {{'neovim/nvim-lspconfig'}} }
+  use 'folke/lsp-colors.nvim'
+  use 'nvim-lua/completion-nvim'
+  -- syntax highlight
+  use { 'nvim-treesitter/nvim-treesitter', run= ':TSUpdate' }
+  -- fuzzy finder
+  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
+  -- key mapping cheatsheet
+  use 'folke/which-key.nvim'
+  -- startup dashboard
+  use 'mhinz/vim-startify'
+  -- markdown
+  use { 'iamcco/markdown-preview.nvim', run = function() vim.fn['mkdp#util#install']() end, ft = {'markdown'}}
+  use 'vhyrro/neorg'
+  -- colorscheme
+  use 'navarasu/onedark.nvim'
+  -- enhanced motion
+  use 'ggandor/lightspeed.nvim'
+end)
