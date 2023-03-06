982951eca798baec260eb30ab144be8ce99853fc
Kush Nee <kushnee5@gmail.com>
2023-03-06 23:44:46 +0800

    update config

--- lua/maps.lua
+++ lua/maps.lua
@@ -32,17 +32,3 @@ vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', {})
 
 -- show which-key
 vim.api.nvim_set_keymap('n', '\'', ':WhichKey<CR>', { noremap=true })
-
--- obsidian
-vim.keymap.set(
-  "n",
-  "gf",
-  function()
-    if require('obsidian').util.cursor_on_markdown_link() then
-      return "<cmd>ObsidianFollowLink<CR>"
-    else
-      return "gf"
-    end
-  end,
-  { noremap = false, expr = true}
-)
