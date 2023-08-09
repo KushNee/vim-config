8a2717bda88c84d481da9e7a26f228435834f717
Kush Nee <kushnee5@gmail.com>
2023-08-09 08:24:57 +0800

    change config for neovide

--- lua/kcn_init.lua
+++ lua/kcn_init.lua
@@ -31,14 +31,14 @@ vim.g.mapleader = ' '
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -47,8 +47,8 @@ require("lazy").setup("plugins")
 -- connect to system clipboard
 vim.opt.clipboard:append("unnamedplus")
 
-local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
-local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
+local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- Set indentation to 2 spaces
 augroup('setIndent', { clear = true })
@@ -61,9 +61,47 @@ autocmd('Filetype', {
 })
 
 if vim.g.neovide then
-    vim.g.neovide_input_macos_alt_is_meta = true
-    vim.g.neovide_cursor_animate_command_line = false
-    vim.g.neovide_cursor_animate_in_insert_mode = false
-    vim.g.neovide_transparency = 0.0
+  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+
+  -- Helper function for transparency formatting
+  local alpha = function()
+    return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
+  end
+
+  -- disable neovide's self black background
+  vim.g.neovide_transparency = 0.0
+
+  local neovide_transparency = false
+  if neovide_transparency then
+    -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
     vim.g.transparency = 0.7
+  else
+    vim.g.transparency = 1.0
+  end
+
+  vim.g.neovide_background_color = "#002b36" .. alpha()
+
+  local function set_ime(args)
+    if args.event:match("Enter$") then
+      vim.g.neovide_input_ime = true
+    else
+      vim.g.neovide_input_ime = false
+    end
+  end
+
+  local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    group = ime_input,
+    pattern = "*",
+    callback = set_ime
+  })
+
+  vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+    group = ime_input,
+    pattern = "*",
+    callback = set_ime
+  })
 end
